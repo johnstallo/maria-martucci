eddccef56eea0bd8420014354da92f59475b7c32
John Stallo <johnstallo@jmba.local>
2018-11-01 23:23:15 -0700

    footer styling to button

--- components/Footer.js
+++ components/Footer.js
@@ -1,20 +1,24 @@
 import Link from 'next/link'
 
-const linkStyle = {
-  fontStyle: 'italic'
-}
-
 const layoutStyle = {
   marginTop: 30, paddingRight: 20
 }
 
 const Footer = (props) => (
     <div className="text-right" style={layoutStyle}>
+        <br/>
         <Link href={props.next}>
-          <a style={linkStyle}>{props.nextTitle} &#8594;</a>
+          <a>{props.nextTitle} &raquo;</a>
         </Link>
         <style jsx>{`
-          
+          a {
+            background-color: #feb236;
+            padding: 8px;
+            color: black;
+            font-size: larger;
+            font-family: 'Merriweather Sans', sans-serif;
+            // box-shadow: 3px 3px #ddd;
+          }
         `}</style>
     </div>
 )
