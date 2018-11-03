20c18b3a40dcd6deed4bee02e71bb4952b24c945
John Stallo <johnstallo@jmba.local>
2018-11-02 19:10:44 -0700

    IT content

--- pages/it/5.js
+++ pages/it/5.js
@@ -1,13 +1,18 @@
 import ChapterLayout from '../../components/ChapterLayout'
 
 export default () => (
-<ChapterLayout
-pageTitle="Maria Martucci - La guarigione di Gino"
-chapterTitle="5. La guarigione di Gino"
-next="/it/6"
-nextTitle="La chiesa di Massafra"
-locale="it">
+  <ChapterLayout
+    pageTitle="Maria Martucci - La guarigione di Gino"
+    chapterTitle="5. La guarigione di Gino"
+    next="/it/6"
+    nextTitle="La chiesa di Massafra"
+    locale="it">
 
-<p>PLACEHOLDER</p>
-</ChapterLayout>
+    <p>Passarono due anni da questa sofferenza e persecuzione. In una notte molto fredda, mia madre sentì una voce che la chiamava dicendo: “Maria, alzati e invoca il mio nome, e io ti farò grazia”. Lei ubbidì e si alzò, nonostante facesse molto freddo: il suo cuore era colmo di calore dello Spirito Santo; si inginocchiò davanti alla culla del bambino e iniziò a pregare il Signore con tutto il suo cuore. Lo invocò dicendo: “Signore, Tu mi hai chiamata e mi hai detto di invocare il Tuo nome e che mi farai grazia. Ti prego Signore! Opera in questa notte, fatti conoscere da quelli che non ti conoscono! Tu sei il Vero Dio, Vivente e Vero, il Re dei Re, il Dottore dei Dottori, opera come Tu vuoi, toglimi da questo calvario, Signore ti prego. Restituiscimi mio figlio, fa’ che torni come prima, oppure prendilo con Te!”. Mentre pregava e piangeva, mio padre le disse: “Ma la vuoi smettere! Anche di notte vai a pregare il diavolo?”.</p>
+    <p>Lei, però, anziché smettere, implorò il nome tre volte Santo del Signore con maggiore forza; e mentre pregava sentì un rumore provenire dalla culla del bambino: era un rumore simile a quello di un pezzo di legno che si spezza, immediatamente seguito da un forte grido di dolore del bambino. Mia madre, con gli occhi ancora chiusi in preghiera, ringraziava il Signore. Era certa e sicura che Dio aveva operato. Si alzò e si affacciò alla culla, quando vide il bambino perfettamente sano, com'era prima della malattia. Con grande gioia lo prese, andò da mio padre e gli disse: “Guarda che grande miracolo ha fatto il Signore questa notte!”. Mio padre ne fu sbalordito tanto che non riusciva più a parlare. Gloria al nome del nostro grande Dio!</p>
+    <p>La mamma avvolse poi il bambino in una coperta e uscì fuori; incominciò a bussare alle porte di tutto il vicinato, urlando: “Aprite! Vedete che grande miracolo ha fatto il Signore!”. La gente, prima di aprire, diceva: “Povera Maria: da quando il figlio si è ammalato è diventata completamente pazza!”.</p>
+    <p>Eppure, presi dalla curiosità, i vicini aprivano la porta e si accorgevano che davvero quel Gesù che tante volte avevano beffato quella notte aveva operato un grande miracolo! Gloria a Dio, perché Lui è il Dio Vivente e Vero!</p>
+    <p>Quella notte, la mia mamma fece un voto al Signore, dicendo: “Da ora in poi, qualsiasi malattia colpirà il mio corpo, Tu sarai il mio Dottore e la mia medicina”.</p>
+    <p>Quel giorno non soltanto Dio guarì il bambino, ma salvò dalla malattia del peccato mio padre e quasi tutta la famiglia materna e paterna. Il Signore salvò anche molta gente del vicinato, e per Sua grazia, la conversione delle anime non si fermò lì, ma continuò ancora a lungo. Proprio come il Signore operava per mezzo degli apostoli quanto più venivano perseguitati (Atti 19:20), anche mia madre si rafforzava nella fede quanto più veniva offesa e vituperata. La gloria per tutto questo va a Dio, che in quelle circostanze così avverse salvava e battezzava le anime con lo Spirito Santo.</p>
+  </ChapterLayout>
 )
--- pages/it/9.js
+++ pages/it/9.js
@@ -1,13 +1,23 @@
 import ChapterLayout from '../../components/ChapterLayout'
 
 export default () => (
-<ChapterLayout
-pageTitle="Maria Martucci - La tubercolosi"
-chapterTitle="9. La tubercolosi"
-next="/it/10"
-nextTitle="La nefrite"
-locale="it">
+  <ChapterLayout
+    pageTitle="Maria Martucci - La tubercolosi"
+    chapterTitle="9. La tubercolosi"
+    next="/it/10"
+    nextTitle="La nefrite"
+    locale="it">
 
-<p>PLACEHOLDER</p>
-</ChapterLayout>
+    <p>Durante il corso della sua vita, mia madre si ammalò molte volte: voglio raccontare anche di questo, per dare gloria a Dio di tutte le volte in cui guarì mia madre, che aveva promesso di fare di Lui il suo Dottore e la sua medicina. </p>
+    <p>Lei aveva tanta fede nel patto che aveva stretto con il Signore il giorno della guarigione di Gino.</p>
+    <p>Una domenica mattina, dopo il culto che si teneva in casa nostra, mia madre si sentì male, le venne da vomitare e così prendemmo una bacinella. Avevo undici anni. Mia madre riempì quella coppa di sangue. Papà chiamò in fretta il medico di famiglia, che conosceva già tutta la testimonianza di mia madre. Le ordinò di mettersi a letto, di non prendere freddo e di fare, il giorno dopo e necessariamente, i raggi al petto e alla spalla perché a lui sembrava una brutta malattia.</p>
+    <p>L’indomani, la mamma andò a farsi i raggi e l’esito fu chiaro: tubercolosi. Il medico voleva farla ricoverare, ma lei rispose: “Dottore, tu sai del voto che ho fatto al Signore”. Ma il medico insisteva, sapendo di quante cure ha bisogno chi si ammala di tubercolosi. “Il Signore è potente e può guarirmi, perché Lui è il mio Dottore, la mia medicina!”, disse infine mia madre, decisa.</p>
+    <p>I giorni passavano e lei era costretta a letto, era debole e non si poteva alzare. In quei frangenti, però, anziché preoccuparsi del suo stato di salute, pregava il Signore perché facesse scendere lo Spirito Santo in casa di zia Lucia, dove i fratelli si incontravano per pregare. E mentre pregava, vide un giovane vestito di bianco, che le passò la mano dietro la spalla e al petto e le disse: “Alzati, vai a pregare anche tu con loro!”. Da quel preciso momento si sentì più forte, si alzò, si vestì e andò a casa di mia zia, dove i fedeli stavano pregando.</p>
+    <p>Entrò in quella casa in punta di piedi, cercando di non farsi notare. Ebbe ancora una visione: una nuvola di sangue copriva i fedeli e una specie di sfera luminosa si muoveva in mezzo alla nuvola. Si inginocchiò e, non potendosi più trattenere, disse: “Fratelli e sorelle, cercate il Signore con tutto il cuore perché questa mattina Egli battezza con lo Spirito Santo”.</p>
+    <p>E veramente, quella mattina, lo Spirito Santo scese in mezzo a loro come il giorno della Pentecoste: parecchi fedeli cominciarono a parlare in altre lingue e furono battezzati con lo Spirito Santo!</p>
+    <p>In quel periodo, ci fu un grande risveglio nella chiesa di Massafra. Anche a Mottola si sparse la voce delle grandi benedizioni che Dio stava accordando ai credenti. Ci fu un grande risveglio in tutta la zona; il Signore salvava, guariva e battezzava nello Spirito Santo. Gloria a Dio per l’opera grande che compie! </p>
+    <p>Dopo pochi giorni, mia madre andò di nuovo dal medico e gli disse: “Dottore, voglio fare di nuovo i raggi ai polmoni”. Il dottore rispose che era troppo presto per rifare i raggi; allora mia madre decise di raccontargli tutto quello che era successo. Il medico, meravigliato, acconsentì a rifarle la richiesta per i raggi. Mia madre quindi andò nuovamente a Taranto, portando con sé le lastre precedenti, in cui si vedevano i segni della tubercolosi. Dopo aver fatto i raggi, il dottore la volle visitare e con grande stupore disse: “Signora, chi le ha detto che ha la tubercolosi?”. Mia madre rispose: “Voi stesso, dottore, mi avete diagnosticato questa malattia”, e gli mostrò le vecchie lastre. Il dottore, quando le vide, rimase ancora più meravigliato di prima, la guardò e disse: “Signora lei ha avuto un grande miracolo!”. A questa affermazione mia madre disse: “Si dottore, avete detto bene, ho avuto un grande miracolo! Si, questo è avvenuto davvero!”, e raccontò al dottore in che modo fosse stata guarita e di come il Signore aveva steso la Sua mano sul suo petto e dietro la spalla; gli raccontò anche del patto che aveva fatto con Dio e della fede che aveva posto in Lui! </p>
+    <p>Quando mia madre parlava di queste cose, non tralasciava mai di annunciare in modo chiaro e semplice anche il messaggio della salvezza in Cristo. È così per tutti quelli che credono in Lui, non possono tacere. Anche quel medico di Taranto, quindi, venne a sapere delle grandi meraviglie che Dio fa per tutti coloro che confidano nel Signore in Spirito e Verità.</p>
+
+  </ChapterLayout>
 )
