658e35d2211bce4779d9f9b8ed4d0af6ceef983d
John Stallo <john.stallo@live.com>
2018-11-05 23:11:29 -0800

    EN translation tweaks

--- pages/12.js
+++ pages/12.js
@@ -8,7 +8,7 @@ export default () => (
     next="/13"
     nextTitle="Conversion of a neighbor">
 
-    <p>At the age of 90 years, my mother developed a stomach ulcer and began to feel unwell. The family doctor told her that if she wanted to live she had to receive surgery; otherwise she would suffer and die. My mother replied to the doctor with these words: “Doctor, I have been serving the Lord for many years and perhaps the time has come for me to go home with Him! The vow I have made with God so many years ago I want to keep until death, because the Lord says: <BibleQuote>Be faithful unto death, and I will give you the crown of life.</BibleQuote>” (See Revelation 2:10). “Doctor, I want the crown that the Lord has promised me!”</p>
+    <p>At the age of 90 years, my mother developed a stomach ulcer and began to feel unwell. The family doctor told her that if she wanted to live she had to have surgery; otherwise she would suffer and die. My mother replied to the doctor with these words: “Doctor, I have been serving the Lord for many years and perhaps the time has come for me to go home to Him! I want to keep until my death the vow I made to God so many years ago, because the Lord says: <BibleQuote>Be faithful unto death, and I will give you the crown of life.</BibleQuote>” (See Revelation 2:10). “Doctor, I want the crown that the Lord has promised me!”</p>
     <p>The doctor was so taken aback that he did not try to persuade her.</p>
   </ChapterLayout>
 )
