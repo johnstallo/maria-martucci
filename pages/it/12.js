0af4a7f1ecea2c5f5192c0cd35f21db04d33bb4e
John Stallo <john.stallo@live.com>
2019-08-18 12:08:07 -0700

    Punctuation

--- pages/it/12.js
+++ pages/it/12.js
@@ -9,7 +9,7 @@ export default () => (
     nextTitle="La conversione di una vicina"
     locale="it">
 
-    <p>All’età 90 anni circa a mia madre fu riscontrata un’ulcera allo stomaco e iniziò a stare male. Il dottore di famiglia le disse che se voleva vivere ancora doveva operarsi e curarsi, altrimenti avrebbe sofferto e sarebbe morta. Mia madre rispose al dottore con queste parole: “Dottore, sono tanti anni che servo il Signore e forse è arrivato il tempo che io me ne vada a casa con Lui! Il patto che ho stretto con Dio tanti anni fa lo voglio mantenere fino alla morte, perché il Signore dice: <BibleQuote>Sii fedele fino alla morte ed io ti darò la corona della Vita.</BibleQuote>” (Cfr. Apocalisse 2:10). “Io, dottore, voglio la corona che il Signore mi ha promesso!”.</p>
+    <p>All’età 90 anni circa a mia madre fu riscontrata un’ulcera allo stomaco e iniziò a stare male. Il dottore di famiglia le disse che se voleva vivere ancora doveva operarsi e curarsi, altrimenti avrebbe sofferto e sarebbe morta. Mia madre rispose al dottore con queste parole: “Dottore, sono tanti anni che servo il Signore e forse è arrivato il tempo che io me ne vada a casa con Lui! Il patto che ho stretto con Dio tanti anni fa lo voglio mantenere fino alla morte, perché il Signore dice: <BibleQuote>Sii fedele fino alla morte ed io ti darò la corona della Vita.</BibleQuote>” (Cfr. Apocalisse 2:10). “Io, dottore, voglio la corona che il Signore mi ha promesso!”</p>
     <p>Il medico rimase così sbalordito che non volle convincerla.</p>
 
   </ChapterLayout>
