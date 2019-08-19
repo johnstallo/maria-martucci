f30a56bd2201174f426774e4d17dc8666f8735e0
John Stallo <john.stallo@live.com>
2019-08-18 22:40:58 -0700

    Text revision

--- pages/preface.js
+++ pages/preface.js
@@ -11,13 +11,13 @@ export default () => (
     <p>We were seven children in my family, and because my mother had already converted to the Gospel I was raised with sound Christian teachings since childhood. My mother tried to instill in us, primarily by example, the love and fear of God who is worthy of being honored in our lives... above all in adversity, because He is faithful and He honors those who honor Him.</p>
     <p>My mother faced every problem with serenity and always trusting in God, and every day we witnessed the Lord invariably working in our household, in both great and small.</p>
     <p>It caused a love to grow in my heart for this amazing Lord who died on the cross even for me and saved me from eternal death.</p>
-    <p>I am deeply thankful to Him for being at my side throughout my life. Together with my husband, Salvatore Convertino, we have served our God with humility and simplicity in all that He has given us to do; we have declared the Gospel in the cities and in the countryside, and by the grace and work of our Lord, we had the joy of seeing the birth of three churches.</p>
+    <p>I am deeply thankful to Him for being at my side throughout my life. Together with my husband, Salvatore Convertino, we have served our God with humility and simplicity in all that He has given us to do; we have declared the Gospel in the cities and in the countryside, and by the grace and work of our Lord we had the joy of seeing the birth of three churches.</p>
       
     <p>God blessed us with six children, and throughout the years we have had moments of joy, and some of difficulty, but we can never be sufficiently grateful for the blessings that He has faithfully bestowed upon us.</p>
     <p>I am now 78 years of age, and about ten years ago a deep desire began to grow in my heart to write down the simple but genuine testimony of my mother; a woman who, even though illiterate, was driven by a strong zeal for her “beloved Savior” (thus she would refer to God), and being filled with the Holy Spirit she testified of God with words and by example until her very last breath.</p>
     <p>I prayed earnestly for the Lord's guidance and I began to write down in a diary all that I could recall about my mother's life. The purpose of this work is to make known to a new generation the faithful dedication of this woman's service to the Lord and how He used her for the salvation of many souls.</p>
-    <p>Throughout her life, my mother never missed an opportunity to share about her powerful Lord and Savior, and now that she can no longer do so, may this little book continue to declare that only in Christ Jesus is there salvation, comfort, and freedom.</p>
-    <p>May the Lord of Glory continue to save men and women in this generation.</p>
+    <p>Throughout her life, my mother never missed an opportunity to share about her mighty Lord and Savior, and now that she can no longer do so, may this little book continue to declare that only in Christ Jesus is there salvation, comfort, and freedom.</p>
+    <p>May the Lord of Glory continue to save men and women in this generation for His faithful service.</p>
     <p><b>- Maria Annunziata Stallo</b></p>
   </ChapterLayout>
 )
