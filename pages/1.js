e4bc98e2dd6c9e837947b7c757a925bd827ca3d9
John Stallo <johnstallo>
2024-08-24 21:37:20 +0800

    Update translation

--- pages/1.js
+++ pages/1.js
@@ -7,17 +7,17 @@ export default () => (
     chapterTitle="1. Introduction"
     next="/2"
     nextTitle="My mother's conversion">
+    
+    <p>My mother, Maria Martucci, was born to Martucci Donato and Lattarulo Rosa Antonia in Mottola, a town in the province of Taranto, on 16th September 1909. Theirs was a large family; her mother worked at home as a seamstress so, as the eldest daughter, the housework and care of the younger brothers were entrusted to Maria who worked with commitment and joy.</p>
 
-<p>Maria Martucci, my mother, was born on September 16, 1909, in Mottola, a town in the province of Taranto, to Donato Martucci and Rosa Antonia Lattarulo. She came from a large family. Her mother was a seamstress who worked from home, and as the eldest daughter, Maria was responsible for the household chores and caring for her younger siblings. She carried out these duties with dedication and joy.</p>
-    
-    <p>At 19, Maria became engaged to my father, Cosimo Stallo, who was born on November 2, 1905, in Massafra to Giovanni Stallo and Maria Annunziata Mellone. He came from a poor family. My paternal grandmother was widowed when my father was only 17 years old; my grandfather was struck by a train while on his way to work with a cart, and he died.</p>
-    
-    <p>After this tragic event, my father took on the role and responsibilities of the head of the family, caring for his home and his four sisters: Antonia, Lucia, Leonarda, and Pompea.</p>
-    
+    <p>My father, Cosimo Stallo, was born to Giovanni Stallo and Maria Annunziata Mellone on 2nd November 1905 in Massafra. His was a poor family. Cosimo was only 17 years old when his father was hit by a train on his way to work and died.</p>
+
+    <p>Being the only boy out of five children - Antonia, Cosimo, Lucia, Leonarda and Pompea - from that moment on, my father began to take care of his home, taking the place and responsibility as the head of the family and real breadwinner.</p>
+        
     <p>When my mother was 20 years old, on November 10, 1929, my parents married and settled in Mottola.</p>
 
     <p>They had three children in the early years of their marriage: a girl and two boys. Sadly, an infectious disease struck the family, claiming the lives of their first two children. After this profound loss, my parents decided to move to my father's hometown of Massafra in 1936, along with their youngest child, Giovanni, who was about two years old at the time.</p>
 
-    <Photo src="../static/maria-martucci-cosimo-stallo.jpg" caption="My parents: Maria Martucci and Cosimo Stallo (1980)" />
+    <Photo src="../static/maria-martucci-cosimo-stallo.jpg" caption="My parents, Maria Martucci and Cosimo Stallo (1980)" />
   </ChapterLayout>
 )
--- pages/2.js
+++ pages/2.js
@@ -8,29 +8,53 @@ export default () => (
     chapterTitle="2. My mother's conversion"
     next="/3"
     nextTitle="The fiery trial">
-    <p>As was common in those days, my mother would often sit outside the front door of their house when she was done with her chores or when it was hot. One day, while sitting there, she saw a young man stop by next door who then called out to the lady who lived there: “Peace!” Now, in her dialect, the word <i>peace</i> (<i>pace</i>) can sound a lot like the word <i>crazy</i> (<i>paccj</i>). My mother did not clearly understand what the young man had meant. Puzzled, she turned to her mother-in-law who was sitting beside her and asked: “Mama, did you hear that young man? Did he say <i>peace</i> or <i>crazy</i>?”</p>
-    <p>Her mother-in-law replied: “My daughter, you must have nothing to do with those people because they are excommunicated, and they do not love the saints!”</p>
-    <p>However, my mother couldn’t get that word she had heard the young man say out of her head, be it <i>peace</i> or <i>crazy</i>. Intrigued, and secretly from her mother-in-law, she later went to her neighbor for an explanation: “Excuse me, ma’am, that young man who greeted you the other day, did he say <i>peace</i> or <i>crazy</i>?”</p>
-    <p>The lady calmly answered: “He said ‘Peace’!” Further intrigued, my mother asked what he had meant by using that word.</p>
-    <p>The lady, who was very affable, explained to her that the young man was an evangelical, and that their greeting meant “peace to you,” the same greeting that Jesus used. The lady went on to tell her that the young evangelical had also given her a Bible (which is the testimony that Jesus left us), but that she never could read it for lack of time.</p>
-    <p>My mother was illiterate, and so she said, “Ma’am, I can’t read, but you can. If you cannot find the time, let’s do this: I can clean your fava beans for you," (that is, she would shell dried fava beans, as was common in those days), "and you read, because I am very interested to know what is written in the Bible.”</p>
-    <p>And so, Angela Maria (the lady's name) accepted the deal and began to read the Bible to her. This arrangement went on for quite a while; they would read the Bible together and then pray.</p>
-    <p>The young man who had greeted Angela Maria with “peace” was called Paolo Spada, and he also being illiterate, began to listen to the Word of God with the two women, and so the three would pray together. The Lord accordingly began to bless them.</p>
-    <p>In 1937, my mother gave birth to another son, whom she called Donato. At that time in Italy, the fascists began to persecute the Pentecostal evangelicals, but believers were also persecuted by the Roman Catholic clergy. There was no Evangelical church in Massafra, and the few evangelical believers gathered in Angela Maria's house.</p>
-    <p>My mother from time to time went to Mottola to visit her family. There was a Baptist church there, which she attended whenever she went to Mottola.</p>
-    <p>Soon after, a preacher named Pola of Ginosa began to visit Massafra.</p>
-    <p>One day as my mother was praying alone in her house, she began to feel a great joy within her: she felt the presence of God in her heart and she began to speak in other tongues; she did not understand what was happening to her, because she had never been told about the Holy Spirit. When later she met with her fellow believers to pray, the Holy Spirit manifested again through my mother speaking in tongues. Sister Angela Maria, who also did not know about the baptism of the Holy Spirit, thought this behavior “strange” and that it was not of God.</p>
-    <p>Several days later, the brother from Ginosa came to visit the believers in Massafra, and Angela Maria immediately referred to him what had happened to my mother during prayer. The sister said: “I don’t know, maybe it is some evil spirit and this thing is not of God.” The brother replied, “Don't worry, I'll examine the matter.”</p>
-    <p>During prayer, my mother began to speak in tongues and there was a great manifestation of the Spirit of God. The brother then explained the bible passage of Pentecost and that this promise was not only for the disciples, but <BibleQuote>unto as many the Lord shall call</BibleQuote> (as it is written in Acts 2) and therefore added: “Sister Maria was baptized in the Holy Spirit!”</p>
-    <p>During that time, my mother was also told about the baptism in water that is in obedience to Jesus’ command to <BibleQuote>go therefore and teach all nations, baptizing them in the name of the Father, and of the Son, and of the Holy Ghost (Matthew 28:19).</BibleQuote> My mother said, “I want to be baptized according to His command!” And so, sister Angela Maria’s bathtub was used as a baptistery, and as it is written that <BibleQuote>joy shall be in heaven over one sinner that repenteth (Luke 15:7)</BibleQuote> so there was great joy also on earth. Glory to God!</p>
-    <p>In 1940, a daughter was born, Maria Annunziata (that is, me, <i>Titina</i>, as they always called me).</p>
-    <p>During that time the Second World War was underway, and it was very difficult to find food, especially for the poorest families; we could buy only flour and other essential food items with food stamps handed out by the local government, and only a specific amount based on the number of members in each household. Food was scarce, and we suffered hunger.</p>
-    <p>In 1942 my mother gave birth to another son, whom she named Antonio; the family continued to grow, and food decreased more and more. My mother prayed to God to provide for them.</p>
-    <p>I remember once... My brother Donato, who was 5 years old at the time, cried, “I’m hungry!” to which my mother would reply: “The Lord shall provide, and we will eat.” At that reply, little Donato exclaimed: “Then let’s pray to God!”</p>
-    <p>My mother, who in her every need trusted in God's help, paid attention to Donato's request, and we all prayed together. We then heard a knock at the front door, and my mother went to open it. It was a neighbor, who said to my mother: “Maria, it was my daughter’s wedding yesterday, and many guests did not come. Many trays of oven-baked pasta, potatoes, and meat were left over, and I thought perhaps you wouldn't be offended if I brought some to you, since you have many children. If you like I will bring everything to you.”</p>
-    <p>We felt so much wonder and joy; from having nothing to eat, all of a sudden we had the table miraculously laden with much delicious food.</p>
-    <p>My mother not only thanked our neighbor and accepted all the good that God had provided, she also told the woman of all that had happened and how the Lord had moved our neighbor's heart in reponse to our prayer.</p>
-    <p>We really saw a miracle of God!</p>
+  
+  <p>As was common in those days, my mother would often sit outside the front door of our house when she was done with her chores or when it was hot. One day, while she was sitting there, she saw a young man stop at the house next door and greet the lady who lived there by saying, “Peace!” In her dialect, the word <i>peace</i> (<i>pace</i>) sounds very similar to the word <i>crazy</i> (<i>paccj</i>). My mother wasn’t sure which word the young man had used. Confused, she turned to her mother-in-law, who was sitting beside her, and asked: “Mama, did you hear that young man? Did he say <i>peace</i> or <i>crazy</i>?”</p>
+
+  <p>Her mother-in-law replied, “My daughter, you should have nothing to do with those people because they are excommunicated. They do not love the saints!”</p>
+
+  <p>However, my mother couldn’t stop thinking about the word she had heard from the young man—whether it was <i>peace</i> or <i>crazy</i>. Intrigued, and careful not to let her mother-in-law know, she later went to her neighbor to clarify: “Excuse me, ma’am, that young man who greeted you the other day, did he say <i>peace</i> or <i>crazy</i>?”</p>
+
+  <p>The lady calmly replied, “He said ‘Peace’!” Still curious, my mother asked what he meant by that word.</p>
+
+  <p>The lady, who was very kind, explained that the young man was an evangelical, and that their greeting, “peace to you,” was the same greeting that Jesus used. She also mentioned that the young evangelical had given her a Bible (which is the testament that Jesus left us), but she never found the time to read it.</p>
+
+  <p>My mother was illiterate, so she said to the lady, “I can’t read, but you can. Since you don’t have the time, let’s do this: I’ll shell your fava beans for you, and you can read the Bible to me. I’m very curious to know what it says.”</p>
+
+  <p>Angela Maria (the lady’s name) accepted the offer, and so they began reading the Bible together. This went on for a long time—they would read the Bible and then pray.</p>
+
+  <p>The young man who had greeted Angela Maria with “peace” was named Paolo Spada, and he was also illiterate. He began to listen to the Word of God with the two women, and the three of them would pray together. The Lord began to bless them.</p>
+
+  <p>In 1937, my mother gave birth to another son, whom she named Donato. At that time in Italy, the Fascists were persecuting Pentecostal evangelicals, and believers were also persecuted by the Roman Catholic clergy. There was no Pentecostal church in Massafra, so the few evangelical believers gathered in Angela Maria’s house.</p>
+
+  <p>My mother occasionally went to Mottola to visit her family. There was a Baptist church there, which she attended whenever she went to Mottola.</p>
+
+  <p>Soon after, a preacher named Pola from Ginosa began visiting Massafra.</p>
+
+  <p>One day, while my mother was praying alone in her house, she began to feel great joy within her—she felt the presence of God in her heart and began to speak in other tongues. She didn’t understand what was happening to her, because she had never been told about the Holy Spirit. Later, when she met with the other believers to pray, the Holy Spirit manifested again through my mother speaking in tongues. Sister Angela Maria, who also didn’t know about the baptism of the Holy Spirit, thought this behavior was “strange” and that it wasn’t from God.</p>
+
+  <p>A few days later, the brother from Ginosa came to visit the believers in Massafra, and Angela Maria immediately told him what had happened to my mother during prayer. She said, “I don’t know, maybe it’s some evil spirit, and this thing isn’t from God.” The brother replied, “Don’t worry, we’ll see.”</p>
+
+  <p>During prayer, my mother began to speak in tongues again, and there was a great manifestation of the Spirit of God. The brother then explained the passage about Pentecost and how the promise was not only for the disciples but for <BibleQuote>everyone whom the Lord our God calls to himself</BibleQuote> (as it is written in Acts 2). He then added: “Sister Maria has been baptized in the Holy Spirit!”</p>
+
+  <p>During that time, my mother also learned about water baptism, which is in obedience to Jesus’ command to <BibleQuote>Go therefore and make disciples of all nations, baptizing them in[a] the name of the Father and of the Son and of the Holy Spirit</BibleQuote> (Matthew 28:19). My mother said, “I want to be baptized according to His command!” And so, Sister Angela Maria’s bathtub was used as a baptistery, and as it is written that there is <BibleQuote>joy in heaven over one sinner who repents</BibleQuote> (Luke 15:7), there was also great joy on earth. Glory to God!</p>
+
+  <p>In 1940, a daughter was born—Maria Annunziata (that is, me, <i>Titina</i>, as they always called me).</p>
+
+  <p>During that time, the Second World War was ongoing, and it was very difficult to find food, especially for the poorest families. We could only buy flour and other essential food items with ration cards handed out by the local government, which allowed us to purchase a specific amount of goods based on the number of members in each household. Food was scarce, and we suffered from hunger.</p>
+
+  <p>In 1942, my mother gave birth to another son, whom she named Antonio. As the family continued to grow, food became even scarcer. My mother prayed to the Lord to provide for us.</p>
+
+  <p>I remember once... My brother Donato, who was 5 years old at the time, cried, “I’m hungry!” To which my mother replied, “The Lord will provide, and we will eat.” At that, little Donato exclaimed, “Then let’s pray to God!”</p>
+
+  <p>My mother, who always trusted in God’s help for every need, immediately responded to Donato’s request, and we all prayed together. Soon after, we heard a knock at the door. My mother went to open it, and it was a neighbor who said to her, “Maria, my daughter got married yesterday, and many guests didn’t show up. We have many trays of baked pasta, potatoes, and meat left over, and I thought you might not mind if I brought some over, since you have so many children. If you want, I’ll bring everything to you.”</p>
+
+  <p>We were amazed and filled with joy; from having nothing to eat, our table was suddenly and miraculously filled with delicious food.</p>
+
+  <p>My mother not only accepted all the food that God had provided, but she also testified to the neighbor about what had happened and how the Lord had moved her heart in response to our prayer. She thanked the neighbor as well.</p>
+
+  <p>We truly witnessed a miracle of God!</p>
+
     <Photo src="../static/mom-and-siblings.jpg" caption="My mother with us children (1996): Mamma, Giovanni, Donato, Maria Annunziata, Antonio, Luigi, Antonietta, Elia" />
   </ChapterLayout>
 )
