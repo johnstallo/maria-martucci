3de9d4134b26c50180196b812e1e2807936b8a34
John Stallo <johnstallo>
2024-08-24 17:54:52 +0800

    Update introduction

--- pages/1.js
+++ pages/1.js
@@ -8,11 +8,16 @@ export default () => (
     next="/2"
     nextTitle="My mother's conversion">
 
-    <p>Maria Martucci was born in Mottola, a town in the province of Taranto, Italy, on September 16, 1909. Her parents were Donato Martucci and Rosa Antonia Latturulo. Hers was a large family: her mother was a seamstress who worked from home, and Maria, being the eldest daughter, was entrusted with the housework and to care for her younger brothers. She worked diligently and with joy.</p>
-    <p>She was 19 years old when she became engaged to my father, Cosimo Stallo (born November 2, 1905 in Massafra to Giovanni Stallo and Maria Annunziata Mellone), who himself came from a poor family. My paternal grandmother was widowed when my father was only 17 years old; a train hit my grandfather as he traveled to work one day, and he died.</p>
-    <p>From that moment onward, my father, who was the only male of five children (Antonia, Cosimo, Lucia, Leonarda, Pompea), dutifully took on the burden and responsibility as head of family.</p>
-    <p>On November 10, 1929, when Maria was 20 years old, my parents married and settled in Mottola.</p>
-    <p>They had three children in the early years of their marriage: a girl and two boys. I do not remember the name of the infectious disease they caught, but the first two children became ill and died within a short timespan of each other. Following this great sorrow, my parents decided to move with their youngest child, Giovanni, who was 2 at the time, to my father’s hometown of Massafra. It was 1936.</p>
+<p>Maria Martucci, my mother, was born on September 16, 1909, in Mottola, a town in the province of Taranto, to Donato Martucci and Rosa Antonia Lattarulo. She came from a large family. Her mother was a seamstress who worked from home, and as the eldest daughter, Maria was responsible for the household chores and caring for her younger siblings. She carried out these duties with dedication and joy.</p>
+    
+    <p>At 19, Maria became engaged to my father, Cosimo Stallo, who was born on November 2, 1905, in Massafra to Giovanni Stallo and Maria Annunziata Mellone. He came from a poor family. My paternal grandmother was widowed when my father was only 17 years old; my grandfather was struck by a train while on his way to work with a cart, and he died.</p>
+    
+    <p>After this tragic event, my father took on the role and responsibilities of the head of the family, caring for his home and his four sisters: Antonia, Lucia, Leonarda, and Pompea.</p>
+    
+    <p>When my mother was 20 years old, on November 10, 1929, my parents married and settled in Mottola.</p>
+
+    <p>They had three children in the early years of their marriage: a girl and two boys. Sadly, an infectious disease struck the family, claiming the lives of their first two children. After this profound loss, my parents decided to move to my father's hometown of Massafra in 1936, along with their youngest child, Giovanni, who was about two years old at the time.</p>
+
     <Photo src="../static/maria-martucci-cosimo-stallo.jpg" caption="My parents: Maria Martucci and Cosimo Stallo (1980)" />
   </ChapterLayout>
 )
