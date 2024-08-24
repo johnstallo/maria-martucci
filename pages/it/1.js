861f9f8a8f4387494738c356d1784c295306dd9a
John Stallo <johnstallo>
2024-08-24 22:05:52 +0800

    Minor grammar tweaks

--- pages/it/1.js
+++ pages/it/1.js
@@ -9,11 +9,11 @@ export default () => (
     nextTitle="La conversione di mia madre"
     locale="it">
 
-    <p>Maria Martucci era nata a Mottola, un paese in provincia di Taranto, il 16 Settembre 1909 da Martucci Donato e Lattarulo Rosa Antonia. La sua era una famiglia numerosa: la madre faceva la sarta in casa, ed essendo Maria la figlia più grande, i lavori domestici e la cura dei fratelli più piccoli erano affidati a lei, che lavorava con impegno e con gioia.</p>
-    <p>Aveva 19 anni quando si fidanzò con mio padre, Cosimo Stallo (nato il 2 Novembre 1905 a Massafra da Stallo Giovanni e Mellone Maria Annunziata), che veniva da una famiglia povera. Mia nonna paterna rimase vedova quando mio padre aveva solo 17 anni; mio nonno fu travolto da un treno mentre si recava a lavoro con il traino, e morì.</p>
-    <p>Da quel momento in poi, mio padre iniziò a prendersi cura della propria casa, prendendo il posto e la responsabilità di un vero e proprio capofamiglia, essendo anche l’unico maschio su cinque figli (Antonia, Cosimo, Lucia, Leonarda, Pompea).</p>
-    <p>Quando mia madre aveva 20 anni, precisamente il 10 Novembre 1929, i miei genitori si sposarono e si stabilirono a Mottola.</p>
-    <p>A Mottola nacquero i primi figli due dei quali, non ricordo da quale malattia infettiva furono colpiti, ma a causa di questa morirono nell’arco di poco tempo… in seguito a questo grande dolore i miei genitori decisero di trasferirsi con il piccolo Giovanni, che era nato nel 1934 e aveva circa due anni a Massafra, paese nativo di mio padre. Era il 1936.</p>
+    <p>Maria Martucci nacque a Mottola, un paese in provincia di Taranto, il 16 settembre 1909, da Martucci Donato e Lattarulo Rosa Antonia. La sua era una famiglia numerosa: la madre faceva la sarta in casa, ed essendo Maria la figlia più grande, i lavori domestici e la cura dei fratelli più piccoli erano affidati a lei, che lavorava con impegno e gioia.</p>
+    <p>Aveva 19 anni quando si fidanzò con mio padre, Cosimo Stallo (nato il 2 novembre 1905 a Massafra da Stallo Giovanni e Mellone Maria Annunziata), che veniva da una famiglia povera. Mia nonna paterna rimase vedova quando mio padre aveva solo 17 anni; mio nonno fu travolto da un treno mentre si recava al lavoro con il traino, e morì.</p>
+    <p>Da quel momento in poi, mio padre iniziò a prendersi cura della propria casa, assumendo il ruolo e la responsabilità di un vero capofamiglia, essendo anche l’unico maschio su cinque figli (Antonia, Cosimo, Lucia, Leonarda, Pompea).</p>
+    <p>Quando mia madre aveva 20 anni, precisamente il 10 novembre 1929, i miei genitori si sposarono e si stabilirono a Mottola.</p>
+    <p>A Mottola nacquero i primi figli, due dei quali, non ricordo quale malattia infettiva li colpì, ma a causa di questa morirono nel giro di poco tempo… In seguito a questo grande dolore, i miei genitori decisero di trasferirsi con il piccolo Giovanni, nato nel 1934 e che aveva circa due anni, a Massafra, paese natale di mio padre. Era il 1936.</p>
     <Photo src="../static/maria-martucci-cosimo-stallo.jpg" caption="I miei genitori: Maria Martucci e Cosimo Stallo (1980)" />
   </ChapterLayout>
 )
